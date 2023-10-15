cd5ed474b109a3fb6d9cc9d232150aa806de5660
Luis Gutierrez <luismarkgo@gmail.com>
2023-10-15 21:29:52 +0200

    Configurado ID como autoincremental

--- resources/schema.sql
+++ resources/schema.sql
@@ -5,7 +5,7 @@ drop table if exists Actividad;
 
 -- Crear la tabla Deportista
 CREATE TABLE Deportista(
-    ID INT PRIMARY KEY,
+    ID INTEGER PRIMARY KEY,
     Nombre VARCHAR(255),
     Apellidos VARCHAR(255),
     CorreoElectronico VARCHAR(255) UNIQUE,
@@ -15,7 +15,7 @@ CREATE TABLE Deportista(
 
 -- Crear la tabla Actividad
 CREATE TABLE Actividad(
-    ID INT PRIMARY KEY,
+    ID INTEGER PRIMARY KEY,
     Fecha DATE,
     DuracionHoras DECIMAL(5, 2),
     Localizacion VARCHAR(255),
