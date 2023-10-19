dba7b09a335ab1827097689243a9f48cbcc956ab
Luis Gutiérrez Llamedo <UO289995@uniovi.es>
2023-10-19 15:56:09 +0200

    Cambios revision semanal

--- src/gestor/gestorModel.py
+++ src/gestor/gestorModel.py
@@ -1,83 +1,64 @@
+from util.database import DataBase
 
-import sys
-from deportista.deportistaModel import DeportistaModel
-import datetime
-
-class GestorView:
+class GestorModel:
     '''
-    Clase que representa la vista (entrada y salida) de datos
-    para las funcionalidades relativas a los deportistas que estarán
-    representadas en la clase DeportistaModel (DeportistaModel.py)
+    Clase que representa los objetos de negocio (model) relativos a las funcionalidades de 
+    los deportistas. Varios métodos de esta clase se invocarán desde la vista (DeportistaView), y los resultados
+    se devolverán nuevamente a la vista para su visualización
     '''
-     
+    
     def __init__ (self):
-        self.deportista = DeportistaModel () #Crea un objeto model que se invocará desde esta vista
-        #Crea un diccionario con las opciones (key) y los métodos/acciones que se pueden realizar en este objeto (values)
-        self.choices = { "1": self.addActivity,
-                         "2": self.showSummary,
-                         "3": self.quit
-                       }
+        self.db = DataBase ("AppDB.db")
     
-    def displayMenu (self):
-        print("#"*20)
-        print(""" Opciones: \n
-              1.- Ver mas activos por orden
-              2.- Ver estado de forma
-              3.- Salir 
-              """)
+    # #Obtiene la lista de empleados para una compañia especificada por su nombre
+    # def getAllEmployees (self,companyName):
+    #     query = """select Company.name as Compania, Employee.id as IdEmpleado, Employee.name as NombreEmpleado, Employee.salary as Salario 
+    #                from Company inner join Employee on Company.id=Employee.idCompany
+    #                where Compania = ? order by Employee.id asc
+    #             """
+    #     return self.db.executeQuery(query,companyName)
     
-    #Muestra la lista de opciones y permite la selección
-    def run (self):
-        while True:
-            self.displayMenu()
-            choice = input("Introducir opción: ")
-            action = self.choices.get(choice)
-            if action:
-                action()
-            else:
-                print("{0} no es una opción valida\n".format(choice))
-  
-    #Vista para la HU Resumen básico sobre mi actividad deportiva a lo largo del tiempo, incluyendo el estado de forma
-    def showSummary(self):
-        '''
-        El deportista se identifica mediante su correo electrónico y se mostrarán sus datos. A continuación, se genera un listado con tipo de actividad, número de sesiones y total distancia, y se mostrará el indicativo sobre el estado de forma del deportista.
-        '''
-        print("#"*20)
-        correoDeportista = input ("Introducir tu correo: ")
-        #Invocación al modelo para obtener el id de la compañia
-        idDeportista = self.deportista.getIdDeportista(correoDeportista)
-        
-        if idDeportista==None:
-            print ("No existe el deportista con correo:",correoDeportista)
-        else:
-            nombre,apellidos = self.deportista.getNombreCompletoDeportista(correoDeportista)
-            print(f"Iniciado sesion: {correoDeportista}")
-            print(f"\n¡Bienvenido {nombre} {apellidos}!" )
-            
-            print("\nResumen de actividad: ")
-            print ("----------------------------------")
+    # #Obtiene resumen (total empleados y media de salario) para una compañia especificada por su nombre
+    # def getSummaryEmployees (self, companyName): 
+    #     query = """select count(Employee.id) as total, avg(Employee.salary) as average
+    #                from Company inner join Employee on Company.id=Employee.idCompany
+    #                where Company.name = ?
+    #                group by (Company.id)
+    #             """
+    #     return  self.db.executeQuery(query,companyName)
+    
+    # #Obtiene el salario medio de todos los empleado por compañia
+    # def getAvgSalary (self):
+    #     query = """select Company.name, avg(Employee.salary) as avgSalary
+    #               from Company left join Employee on Company.id=Employee.idCompany
+    #               group by (Company.id)
+    #             """
+    #     return self.db.executeQuery(query)
+    
+    #Inserción de lo datos de un empleado (name,salary,birthdate) en una compañia
+    #Notar que no es necesario indicar explícitamente el valor de la clave del empleado (id),
+    # #ya que cuando éste es null, sqlite lo genera de forma autoincremental
+    # def insertEmploye (self,name,salary,birthDate,idCompany):
+    #     query = """
+    #             insert into Employee(id,name,salary,birthDate,idCompany) values (null,?,?,?,?) 
+    #             """
+    #     self.db.executeUpdateQuery(query,name,salary,birthDate,idCompany)
+    
 
-            #Invocación al modelo para obtener el resumen
-            res = self.deportista.getSummary(idDeportista)
-            self.printResults(res)
-            
-            
-    def quit(self):
-        print("Cerrando opciones.")
-        sys.exit(0)
+    #Obtiene el id de un deportista especificado por su correo.
+   
+   def gestorEstado(self):
+        consulta_total_activos = "SELECT COUNT(*) FROM Deportista;"
+        total_activos = self.db.executeQuery(consulta_total_activos)
 
-    #Médodo muy general para imprimir los resultados (res) que vienen del model
-    def printResults (self,res):
-        if len(res)==0: 
-            print ("No hay resultados")
-        else:
-            #cabecera
-            print (list(res[0].keys())) # Imprime nombres de columnas (keys del diccionario)
-            print ("----------------------------------")
-            #contenido 
-            for row in res:
-                print ([*row.values()]) # Imprime valor de una fila (values del diccionario)
-            print ("----------------------------------")
+        consulta_perfil = """
+                        SELECT
+                            SUM(CASE WHEN Premium = 1 THEN 1 ELSE 0 END) AS TotalPremium,
+                            SUM(CASE WHEN Premium = 0 THEN 1 ELSE 0 END) AS TotalFree
+                        FROM Deportista;
+                        """     
+        resultados_perfil = self.db.executeQuery(consulta_perfil)
+        total_premium = resultados_perfil[0]
+        total_free = resultados_perfil[1]
 
-
-        
+        return [total_activos,total_free,total_premium]
