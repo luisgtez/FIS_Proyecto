74fb3b182d3ca1f8ab6329b47c6db85110e96e22
Luis Gutierrez <luismarkgo@gmail.com>
2023-11-28 22:24:17 +0100

    Añadido HU

--- src/GUI/view.py
+++ src/GUI/view.py
@@ -2,7 +2,6 @@ import sys
 from deportista.deportistaView import DeportistaView
 from gestor.gestorView import GestorView
 from entidad.entidadView import EntidadView
-import datetime
 
 class View:
     '''
@@ -18,43 +17,56 @@ class View:
         #Crea un diccionario con las opciones (key) y los métodos/acciones que se pueden realizar en este objeto (values)
         self.choices = {
                         "0":self.deportista.showRegistro,
-                        "1":self.entidad.showRegistro,
+                        "1": self.deportista.addActivity,
+                        "2": self.deportista.inscribir_actividad,
+                        "3": self.deportista.importar_csv_view,
+                        "4": self.deportista.showSummary,
+                        "5": self.deportista.showActividadesEnPeriodo,
+                        "6": self.deportista.addObjetivoSemanalView,
+                        
+                        "7": self.deportista.showActividadesDeportistaTipo,
+                        "8": self.deportista.showComparacion,
+                        
+                        "9":self.entidad.showRegistro,
+                        "10": self.entidad.addActividadEntidad,
                             
-                        "2": self.deportista.addActivity,
-                        "3": self.entidad.addActividadEntidad,
-                        "4": self.deportista.importar_csv_view,
-                        "5": self.deportista.showSummary,
-                        "6": self.deportista.showActividadesEnPeriodo,
-                        "7": self.deportista.addObjetivoSemanalView,
-                        "8": self.deportista.showActividadesDeportistaTipo,
-                        "9": self.deportista.showComparacion,
-                        "10": self.gestor.gestorEstadoview,
-                        "11": self.gestor.masActivos,
-                        "12": self.gestor.añadirTipoActividad_view,
-                        "13": self.gestor.añadirSubtipoActividad_view,
-                        "14": self.quit
+                        "11": self.gestor.gestorEstadoview,
+                        "12": self.gestor.masActivos,
+                        "13": self.gestor.añadirTipoActividad_view,
+                        "14": self.gestor.añadirSubtipoActividad_view,
+                        
+                        "15": self.quit
                           }
         
     def displayMenu (self):
         print("#"*20)
         print(""" Opciones: \n
+              ----- Deportista ---------------------------------
               0.- Registrar nuevo deportista
-              1.- Registrar nueva entidad (Entidad)
-               
-              2.- Registrar nueva actividad
-              3.- Registrar nueva actividad (Entidad)
-              4.- Importar actividades CSV
-              5. -Resumen de actividad
-              6.- Resumen de actividad entre dos fechas
-              7.- Añadir objetivo semanal
-              8.- Resumen de actividad de deportista por tipo (Premium)
-              9.- Comparar con deportista (Premium)
-              10.- Datos Deportistas (Gestor)
-              11.- Deportistas más activos (Gestor)
-              12.- Añadir Tipo de Actividad (Gestor)
-              13.- Añadir Subtipo de Actividad (Gestor)
-              12.- Salir
+              
+              1.- Registrar nueva actividad
+              2.- Inscribirme en una actividad programada
+              3.- Importar actividades CSV
+              4.- Resumen de actividad
+              5.- Resumen de actividad entre dos fechas
+              6.- Añadir objetivo semanal
+              
+              ----- Deportista Premium ------------------------
+              7.- Resumen de actividad de deportista por tipo
+              8.- Comparar con deportista
+
+              ----- Entidad -----------------------------------
+              9.- Registrar nueva entidad
+              10.- Registrar nueva actividad programada
+              
+              ----- Gestor ------------------------------------
+              11.- Datos Deportistas
+              12.- Deportistas más activos
+              13.- Añadir Tipo de Actividad
+              14.- Añadir Subtipo de Actividad
               """)
+        print("#"*20)
+        print("15.- Salir")
         
     #Muestra la lista de opciones y permite la selección
     def run (self):
